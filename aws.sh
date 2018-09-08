4749903b7aa4e650c29eb4ef7648160c9941ad9d
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2018-09-08 15:25:44 -0300

    clean scripts.

--- aws.sh
+++ aws.sh
@@ -12,17 +12,7 @@ export BASEDIR CONFDIR SSHCONF
 
 export DISPLAY_SKIPPED_HOSTS="false"
 
-#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "create_rds_instances" AWS.yml
-#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "create_security_groups" AWS.yml
-#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "gather_ec2" AWS.yml
-#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "create_security_groups, show_debug" AWS.yml
-#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "create_security_groups, create_key_pairs" AWS.yml
-#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "gather_default_vpc, gather_ec2" AWS.yml
-#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "change_state_all_ec2_instances, change_state_ec2_instances" AWS.yml
-#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "change_state_ec2_instances" AWS.yml
-#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "delete_ec2_instances" AWS.yml
-#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "create_ec2_instances,change_state_all_ec2_instances,change_state_ec2_instances" AWS.yml
-#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "config_ansible_host_file, gather_ec2" AWS.yml
+#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "base_config" AWS.yml
 ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" AWS.yml
 
 rm -f *.retry
--- uninst.sh
+++ uninst.sh
@@ -17,7 +17,7 @@ export DISPLAY_SKIPPED_HOSTS="false"
 
 export ANSIBLE_SSH_ARGS="-C -o ControlMaster=auto -o ControlPersist=60s -F ${SSHCONF}"
 
-ansible-playbook -i hosts --extra-vars "gather_y_n=no update_cache_y_n=no purge_y_n=yes autoremove_y_n=yes basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" removeDrupal.yml
-#ansible-playbook -i hosts --extra-vars "gather_y_n=no update_cache_y_n=no purge_y_n=yes autoremove_y_n=yes basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --skip-tags "uninstall_dep_pkg" removeDrupal.yml
+#ansible-playbook -i hosts --extra-vars "gather_y_n=no update_cache_y_n=no purge_y_n=yes autoremove_y_n=yes basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" removeDrupal.yml
+ansible-playbook -i hosts --extra-vars "gather_y_n=no update_cache_y_n=no purge_y_n=yes autoremove_y_n=yes basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --skip-tags "uninstall_dep_pkg" removeDrupal.yml
 
 rm -f *.retry
