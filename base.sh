de6dcdea9988282657348acbe9bd123f29b79aad
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2018-09-12 15:39:00 -0300

    Created base linux roles.

---
+++ base.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+PLAYDIR="`dirname $0`"
+
+cd "$PLAYDIR"
+
+BASEDIR="`pwd`"
+CONFDIR="${BASEDIR}/conf.d"
+SSHCONF="${CONFDIR}/ssh_config"
+
+# Ensure priv keys in conf.d dir are protected
+chmod 600 ${CONFDIR}/*
+
+export BASEDIR CONFDIR SSHCONF
+
+export DISPLAY_SKIPPED_HOSTS="false"
+
+export ANSIBLE_SSH_ARGS="-C -o ControlMaster=auto -o ControlPersist=60s -F ${SSHCONF}"
+
+# Use --tags "__TAG_NAME__" to restrict ansible-playbook in the following useful tags:
+#  - base_users
+#  - auth_keys
+#  - deploy_templates
+#  - install_dep_pkg
+
+#ansible-playbook -i hosts --extra-vars "gather_y_n=false update_cache_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "base_users" Base.yml
+#ansible-playbook -i hosts --extra-vars "gather_y_n=false update_cache_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --skip-tags "install_dep_pkg" Base.yml 
+ansible-playbook -i hosts --extra-vars "gather_y_n=false update_cache_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" Base.yml 
+
+rm -f *.retry
