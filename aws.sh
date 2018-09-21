30998764d1fd12f24ef6576bd2c47a4adf7a84fe
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2018-09-21 13:40:20 -0300

    More general aws.sh script.

--- aws.sh
+++ aws.sh
@@ -1,5 +1,41 @@
 #!/bin/sh
 
+# Check base packages to continue
+which sudo > /dev/null 2>&1
+[ "$?" != 0 ] && ( echo "You must install sudo to run this script"; exit 1 )
+
+which pip > /dev/null 2>&1
+if [ "$?" != 0 ]; then
+   sudo apt install -y python-pip
+fi
+
+# Check release
+[ -f /etc/os-release ] && . /etc/os-release
+
+which ansible > /dev/null 2>&1
+if [ "$?" != 0 ]; then
+  case "$ID" in
+    'debian')
+       grep "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" /etc/apt/sources.list > /dev/null 2>&1 
+       if [ "$?" != 0 ]; then
+          sudo sh -c "echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list"
+       fi
+       # Followed instructions in https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#latest-releases-via-apt-debian
+       sudo apt install dirmngr
+       sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+       sudo apt-get update
+       sudo apt-get -y install ansible
+    ;;
+    'ubuntu')
+       sudo apt-get update
+       sudo apt-get -y install software-properties-common
+       sudo apt-add-repository ppa:ansible/ansible
+       sudo apt-get update
+       sudo apt-get -y install ansible
+    ;;
+  esac
+fi
+
 PLAYDIR="`dirname $0`"
 
 cd "$PLAYDIR"
@@ -14,11 +50,11 @@ export DISPLAY_SKIPPED_HOSTS="false"
 
 # First check base local pre-requisites, but becoming root with password:
 echo "Local sudo to install local base requirements (may comment line after first run)"
-ansible-playbook --ask-become-pass base_AWS.yml
+ansible-playbook -i hosts --ask-become-pass base_AWS.yml
 # can comment the line after first successfull run
 
 # The rest of AWS stuff may work with a local non-root user
-#ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "base_config" AWS.yml
-ansible-playbook --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" AWS.yml
+#ansible-playbook -i hosts --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "base_config" AWS.yml
+ansible-playbook -i hosts --extra-vars "gather_y_n=false basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" AWS.yml
 
 rm -f *.retry
