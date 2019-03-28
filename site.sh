8018b4aa94ba22120b77ffc1b82973787d7855da
Luiz Carvalho <luizrogeriocarvalho@gmail.com>
2019-03-28 14:18:53 -0300

    Some fixes in variable definition. Organized templates. Added group_vars/php_servers.

--- site.sh
+++ site.sh
@@ -1,5 +1,37 @@
 #!/bin/sh
 
+# Check base packages to continue
+which sudo > /dev/null 2>&1
+[ "$?" != 0 ] && ( echo "You must install sudo to run this script"; exit 1 )
+
+# Check release
+[ -f /etc/os-release ] && . /etc/os-release
+
+which ansible > /dev/null 2>&1
+if [ "$?" != 0 ]; then
+  case "$ID" in
+    'debian')
+       grep "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" /etc/apt/sources.list > /dev/null 2>&1 
+       if [ "$?" != 0 ]; then
+          sudo sh -c "echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list"
+       fi
+       # Followed instructions in https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#latest-releases-via-apt-debian
+       sudo apt install dirmngr
+       sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+       sudo apt-get update
+       sudo apt-get -y install ansible
+    ;;
+    'ubuntu')
+       sudo apt-get update
+       sudo apt-get -y install software-properties-common
+       sudo apt-add-repository ppa:ansible/ansible
+       sudo apt-get update
+       sudo apt-get -y install ansible
+    ;;
+  esac
+fi
+
+
 PLAYDIR="`dirname $0`"
 
 cd "$PLAYDIR"
@@ -48,7 +80,7 @@ export ANSIBLE_SSH_ARGS="-C -o ControlMaster=auto -o ControlPersist=60s -F ${SSH
 #ansible-playbook -i hosts --extra-vars "gather_y_n=false update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --skip-tags "python_bootstrap,install_dep_pkg,base_users,drupal_site,php_config,ssl_certificate" Site.yml 
 # Only update certs
 #ansible-playbook -i hosts --extra-vars "gather_y_n=false update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --skip-tags "python_bootstrap,update_repository,install_dep_pkg,base_users,drupal_site,php_config,databases,cron_config" Site.yml 
-#ansible-playbook -i hosts --extra-vars "gather_y_n=false update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --skip-tags "python_bootstrap" Site.yml 
-ansible-playbook -i hosts --extra-vars "gather_y_n=false update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "php_config,final_config" Site.yml 
+ansible-playbook -i hosts --extra-vars "gather_y_n=false update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --skip-tags "python_bootstrap" Site.yml 
+#ansible-playbook -i hosts --extra-vars "gather_y_n=false update_cache_y_n=no basedir=${BASEDIR} confdir=${CONFDIR} sshconf=${SSHCONF}" --tags "php_config,final_config" Site.yml 
 
 rm -f *.retry
